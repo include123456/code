4e2b51776bec771bba0e2dd21b679c6e9665abca
shuyi <shuyi>
2019-01-18 14:17:26 +0800

    添加freemarker与ojdbc

--- src/main/java/com/sinovatech/test/Test.java
+++ src/main/java/com/sinovatech/test/Test.java
@@ -1,4 +1,4 @@
-package com.sinovatech.model;
+package com.sinovatech.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -11,8 +11,11 @@ public class Test {
     public static void main(String[] args) {
 
         ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
-        Model model = (Model)ac.getBean("model");
-        model.print();
+
+        // 读取model模板
+
+
+
 
     }
 }
