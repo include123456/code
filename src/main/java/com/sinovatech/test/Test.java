974f0d0433118fc95ceb269c4ad5a04a3b89d8b7
shuyi <shuyi>
2019-01-18 15:42:23 +0800

    增加lombok

--- src/main/java/com/sinovatech/test/Test.java
+++ src/main/java/com/sinovatech/test/Test.java
@@ -1,21 +1,30 @@
 package com.sinovatech.test;
 
+import com.sinovatech.util.FreeMarkerFactory;
+import freemarker.template.TemplateException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author shuyi
  * @date 2019年1月16日 下午8:40:08
  */
 public class Test {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException, TemplateException {
 
         ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
+        FreeMarkerFactory factory = (FreeMarkerFactory) ac.getBean("freeMarkerFactory");
+        HashMap<String, String> map = new HashMap<String, String>();
+        //
+        map.put("hello","xxxxx");
 
-        // 读取model模板
-
-
-
+        //
+        factory.createFile("hbm",map,".xml");
 
     }
 }
