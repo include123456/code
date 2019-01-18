c5c5801885312b4b66081e273ba682db7d0482e9
shuyi <shuyi>
2019-01-18 11:43:39 +0800

    修改config

--- src/main/java/com/sinovatech/config/Application.java
+++ src/main/java/com/sinovatech/config/Application.java
@@ -3,19 +3,16 @@ package com.sinovatech.config;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 @Configuration
+@PropertySources({
+        @PropertySource("classpath:config.properties")
+})
 public class Application {
 
-    @Bean
-    public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer(String config) {
-        PropertyPlaceholderConfigurer configurer = new PropertyPlaceholderConfigurer();
-        configurer.setIgnoreUnresolvablePlaceholders(true);
-        Resource locations = new ClassPathResource(config);
-        configurer.setLocations(locations);
-        return configurer;
-    }
 
 }
--- src/main/java/com/sinovatech/model/Test.java
+++ src/main/java/com/sinovatech/model/Test.java
@@ -10,13 +10,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
 
-        @SuppressWarnings("resource")
         ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
         Model model = (Model)ac.getBean("model");
         model.print();
-
-        String s = (String)ac.getBean("config");
-        System.out.println(s);
 
     }
 }
