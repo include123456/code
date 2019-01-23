7419492b48cdb07c246d3bf5b16a3abf7b885c6c
shuyi <shuyi>
2019-01-23 14:36:32 +0800

    优化

--- src/main/java/com/test/entity/BaseFreeMarker.java
+++ src/main/java/com/test/entity/BaseFreeMarker.java
@@ -4,8 +4,22 @@ import java.util.Map;
 
 public interface BaseFreeMarker {
 
+    /**
+     * 创建文件
+     * 
+     * @param model
+     * @param map
+     * @param fileType
+     * @throws Exception
+     */
     void createFile(String model, Map map, String fileType) throws Exception;
 
+    /**
+     * 获取hbm文件属性
+     * 
+     * @return
+     * @throws Exception
+     */
     Hbm getHbm() throws Exception;
 
 }
--- src/main/java/com/test/util/Consts.java
+++ src/main/java/com/test/util/Consts.java
@@ -15,13 +15,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class Consts {
 
-    public static final Map<String, String> configMap = new HashMap<String, String>();
+    public static final Map<String, String> CONFIG_MAP = new HashMap<String, String>();
 
     static {
-        configMap.put(String.class.getName(), "VARCHAR2");
-        configMap.put(Date.class.getName(), "DATE");
-        configMap.put(Double.class.getName(), "NUMBER");
-        configMap.put(Integer.class.getName(), "NUMBER");
+        CONFIG_MAP.put(String.class.getName(), "VARCHAR2");
+        CONFIG_MAP.put(Date.class.getName(), "DATE");
+        CONFIG_MAP.put(Double.class.getName(), "NUMBER");
+        CONFIG_MAP.put(Integer.class.getName(), "NUMBER");
     }
 
 }
